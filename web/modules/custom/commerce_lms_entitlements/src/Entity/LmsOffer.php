d9a8513bd64fedd4e5546c476bd4f138fc672b58
leeksoup <nerdgal7501@gmail.com>
2026-09-05 23:33:28 -0500

    Fix LMS offer entity handler annotations

--- web/modules/custom/commerce_lms_entitlements/src/Entity/LmsOffer.php
+++ web/modules/custom/commerce_lms_entitlements/src/Entity/LmsOffer.php
@@ -13,8 +13,8 @@ use Drupal\Core\Config\Entity\ConfigEntityBase;
  *   id = "commerce_lms_offer",
  *   label = @Translation("Commerce LMS offer"),
  *   handlers = {
- *     "list_builder" = "Drupal\\Core\\Config\\Entity\\ConfigEntityListBuilder",
- *     "form" = {"add" = "Drupal\\commerce_lms_entitlements\\Form\\OfferForm", "edit" = "Drupal\\commerce_lms_entitlements\\Form\\OfferForm", "delete" = "Drupal\\Core\\Entity\\EntityDeleteForm"}
+ *     "list_builder" = "Drupal\Core\Config\Entity\ConfigEntityListBuilder",
+ *     "form" = {"add" = "Drupal\commerce_lms_entitlements\Form\OfferForm", "edit" = "Drupal\commerce_lms_entitlements\Form\OfferForm", "delete" = "Drupal\Core\Entity\EntityDeleteForm"}
  *   },
  *   config_prefix = "offer",
  *   admin_permission = "administer commerce lms offers",
